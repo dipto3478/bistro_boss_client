07607c4c1f69f7d60130c182dc5d56c7a411c982
dipto3478 <kitkatboy3478@gmail.com>
2023-06-25 18:40:38 +0600

    our menu section done

--- src/pages/Home/PopularMenu/PopularMenu.jsx
+++ src/pages/Home/PopularMenu/PopularMenu.jsx
@@ -1,45 +1,30 @@
-
-
 import { useEffect, useState } from "react";
 import SectionTitle from "../../../Shared/SectionTitle";
 import MenuItem from "../../../Shared/MenuItem";
-
-
-
-
+import useMenu from "../../../hooks/useMenu";
 
 const PopularMenu = () => {
-    const [popular , setPopular] = useState([])
-    
-    useEffect(() => {
-        fetch('menu.json')
-        .then(res => res.json())
-        .then(data => {
-            const popular = data.filter(item => item.category === "popular")
-            setPopular(popular)
-        })
-    }, [])
-    
-    return (
-        <section className="mb-12">
-            <SectionTitle
-                heading="From Our Menu"
-                subHeading="Popular Items"
-            ></SectionTitle>
-            <div className="grid md:grid-cols-2 gap-10">
-                {
-                    popular.map(item => <MenuItem
-                        key={item._id}
-                        item={item}
-                    ></MenuItem>)
-                }
-            </div>
-            <div className="text-center">
+  const [menu] = useMenu();
+  const popular = menu.filter((item) => item.category === "popular");
 
-            <button className="btn btn-outline  border-0 border-b-4 mt-4">View Full Menu</button>
-            </div>
-        </section>
-    );
+  return (
+    <section className="mb-12">
+      <SectionTitle
+        heading="From Our Menu"
+        subHeading="Popular Items"
+      ></SectionTitle>
+      <div className="grid md:grid-cols-2 gap-10">
+        {popular.map((item) => (
+          <MenuItem key={item._id} item={item}></MenuItem>
+        ))}
+      </div>
+      <div className="text-center">
+        <button className="btn btn-outline  border-0 border-b-4 mt-4">
+          View Full Menu
+        </button>
+      </div>
+    </section>
+  );
 };
 
 export default PopularMenu;
