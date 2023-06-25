e60b741a3dcdac247292fd173c5280a03129e283
dipto3478 <kitkatboy3478@gmail.com>
2023-06-25 15:03:40 +0600

    home section done

--- src/Shared/NavBar.jsx
+++ src/Shared/NavBar.jsx
@@ -1,42 +1,40 @@
-import {  Menu, X } from 'lucide-react'
-import  { useState } from 'react'
-import {Link} from "react-router-dom";
-const menuItems = <>
-<li className="-m-3 flex items-center rounded-md p-3 text-sm font-semibold hover:bg-gray-50">
-    <Link to="/">Home</Link>
-</li>
-<li className="-m-3 flex items-center rounded-md p-3 text-sm font-semibold hover:bg-gray-50">
-    <Link to="/">Home</Link>
-</li>
-<li className="-m-3 flex items-center rounded-md p-3 text-sm font-semibold hover:bg-gray-50">
-    <Link to="/">Home</Link>
-</li>
-<li className="-m-3 flex items-center rounded-md p-3 text-sm font-semibold hover:bg-gray-50">
-    <Link to="/">Home</Link>
-</li>
-</>
-  
-  
-const NavBar = () => {
-    const [isMenuOpen, setIsMenuOpen] = useState(false)
-  
-    const toggleMenu = () => {
-      setIsMenuOpen(!isMenuOpen)
-    }
+import { Menu, X } from "lucide-react";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+const menuItems = (
+  <>
+    <li className="-m-3 flex items-center rounded-md p-3 text-sm font-semibold hover:bg-gray-50">
+      <Link to="/">Home</Link>
+    </li>
+    <li className="-m-3 flex items-center rounded-md p-3 text-sm font-semibold hover:bg-gray-50">
+      <Link to="/">Home</Link>
+    </li>
+    <li className="-m-3 flex items-center rounded-md p-3 text-sm font-semibold hover:bg-gray-50">
+      <Link to="/">Home</Link>
+    </li>
+    <li className="-m-3 flex items-center rounded-md p-3 text-sm font-semibold hover:bg-gray-50">
+      <Link to="/">Home</Link>
+    </li>
+  </>
+);
 
-    return (
-        <div className="fixed  w-full bg-white  z-10">
+const NavBar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  return (
+    <div className="fixed  w-full bg-white  z-10">
       <div className="mx-auto flex max-w-7xl items-center justify-between px-4 py-2 sm:px-6 lg:px-8">
         <div className="inline-flex items-center space-x-2">
-          
-          <span className="font-bold">DevUI</span>
+          <span className="font-bold text-xl">Bistro Boss</span>
         </div>
         <div className="hidden grow lg:flex">
-          <ul className="ml-12 inline-flex space-x-8">
-            {menuItems}
-          </ul>
+          <ul className="ml-12 inline-flex space-x-8">{menuItems}</ul>
         </div>
-        
+
         <div className="lg:hidden">
           <Menu onClick={toggleMenu} className="h-6 w-6 cursor-pointer" />
         </div>
@@ -46,8 +44,7 @@ const NavBar = () => {
               <div className="px-5 pb-6 pt-5">
                 <div className="flex items-center justify-between">
                   <div className="inline-flex items-center space-x-2">
-                 
-                    <span className="font-bold">DevUI</span>
+                    <span className="font-bold">Bistro Boss</span>
                   </div>
                   <div className="-mr-2">
                     <button
@@ -61,18 +58,15 @@ const NavBar = () => {
                   </div>
                 </div>
                 <div className="mt-6">
-                  <nav className="grid gap-y-4">
-                    {menuItems}
-                  </nav>
+                  <nav className="grid gap-y-4">{menuItems}</nav>
                 </div>
-                
               </div>
             </div>
           </div>
         )}
       </div>
     </div>
-    );
+  );
 };
 
 export default NavBar;
--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { HelmetProvider } from "react-helmet-async";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./routes/routes";
 
-import {
-  RouterProvider,
-} from "react-router-dom";
-import { router } from './routes/routes';
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-<div className='mx-auto max-w-screen-xl'>
-<React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
-</div>
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <div className="mx-auto max-w-screen-xl">
+    <React.StrictMode>
+      <HelmetProvider>
+        <RouterProvider router={router} />
+      </HelmetProvider>
+    </React.StrictMode>
+  </div>
+);
